942c282d67fef6ab17238ce936b9994a483e9f51
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2022-02-27 08:22:31 +0530

    Add files via upload

---
+++ player/js/features/notifier.js
@@ -0,0 +1,15 @@
+var noti=op(".noti"),timxx=false,
+conN=true,
+notiTop=op(".noti .top");
+
+notiTop.onclick=()=>{
+	noti.classList.toggle("active");
+	clearTimeout(timxx);
+	if(!noti.classList.contains("active") && conN){
+		send("/...Closed Notifier");
+	}
+}
+timxx=setTimeout(()=>{
+	noti.classList.add("active");
+},5000);
+
