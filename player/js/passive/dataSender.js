d747688c9ffa38ca2473669df4843f6e52e8a16b
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2022-03-05 17:23:07 +0530

    Add files via upload

--- player/js/passive/dataSender.js
+++ player/js/passive/dataSender.js
@@ -1,4 +1,4 @@
-let shareName=["Team of fun","Awesome Code","Chahat","Telegram","Prashant","Swapnil","Akash","Josh","fb","insta","other","qr code","person","code viewer site","ai kit","WISH","user share","Spin&Win"],
+let shareName=["Team of fun","Awesome Code","Chahat","Telegram","Prashant","Swapnil","Akash","Josh","fb","insta","other","qr code","person","code viewer site","ai kit","WISH","user share","Spin&Win","MallQr"],
 aiSharedBy;
 
 setInterval(()=>{
