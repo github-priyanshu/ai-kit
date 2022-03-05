4611367764a8f9d5f34b114521e988c3e83eda1d
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2022-03-05 17:11:05 +0530

    Update sw.js

--- player/sw.js
+++ player/sw.js
@@ -1,4 +1,1 @@
 self.addEventListener('fetch', (event) => {});
-
-/* PROPELLER ADS*/
-function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://iclickcdn.com/tag.min.js',4903243,document.body||document.documentElement
