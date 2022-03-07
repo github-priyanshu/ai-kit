0c5a608568db5b353db174d5c24bb8988376d44c
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2022-03-07 07:36:34 +0530

    Update a-adsHtml.js

--- player/js/a-adsHtml.js
+++ player/js/a-adsHtml.js
@@ -1,1 +1,1 @@
-var aadsHtml=`<iframe data-aa='1945725' src='//ad.a-ads.com/1945725?size=336x280' style='width:336px; height:280px; border:0px; padding:0; overflow:hidden; background-color: transparent;'></iframe>`;
+var aadsHtml=`<iframe data-aa='1952232' src='//ad.a-ads.com/1952232?size=336x280' style='width:336px; height:280px; border:0px; padding:0; overflow:hidden; background-color: transparent;'></iframe>`;
