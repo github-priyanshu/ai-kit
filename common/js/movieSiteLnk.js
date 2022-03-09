94540e385de64add592146f3755250a8597abcb1
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2022-03-09 18:25:36 +0530

    Update movieSiteLnk.js

--- common/js/movieSiteLnk.js
+++ common/js/movieSiteLnk.js
@@ -1,2 +1,2 @@
 var ms="https://dl.odcloud01.workers.dev/",
-errorMsg="/...Server Down. Movies are not working. It will be resolved very soon";
+errorMsg='/...Server Down. Movies are not working. It will be resolved very soon <a href="https://t.me/aiplayer_movies">JOIN TELEGRAM FOR MORE UPDATES</a>';
