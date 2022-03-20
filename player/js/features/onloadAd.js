bb5206e60250590ac0bee0598599c1fcaeacf064
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2022-03-20 12:04:02 +0530

    Add files via upload

--- player/js/features/onloadAd.js
+++ player/js/features/onloadAd.js
@@ -1,12 +1,26 @@
 /*PROPELLAR ADS FUNCITONS*/
-var onPlayAd=false;
-function openProAd(){	
-	if(aiLoadedNum>1 && !onPlayAd){
-		onPlayAd=true;
+
+var proIntAdList=[
+{"async":"async","data-cfasync":"false","src":"//upgulpinon.com/1?z=4952593"},
+{"async":"async","data-cfasync":"false","src":"//upgulpinon.com/1?z=4954236"},
+{"async":"async","data-cfasync":"false","src":"//upgulpinon.com/1?z=4954239"},
+{"async":"async","data-cfasync":"false","src":"//upgulpinon.com/1?z=4954240"},
+]
+
+var lastIntAdTime=localStorage.getItem("lastIntAdTime") || 0;
+lastIntAdTime=Number(lastIntAdTime);
+
+function openProAd(){
+	var nowTime=new Date().getTime()/1000;
+	log(nowTime)
+
+	if(aiLoadedNum>1 && lastIntAdTime+120 < nowTime && proIntAdList.length){
+		lastIntAdTime=nowTime;
 		log("open");
-		setTimeout(()=>{
-			makeScript({"async":"async","data-cfasync":"false","src":"//upgulpinon.com/1?z=4952593"});
-		},1000)
+		makeScript(proIntAdList.shift());
+		localStorage.setItem("lastIntAdTime",nowTime)
+		send("/... Shown pro ad "+ 5 - proIntAdList.length);
 	}
 }
 setTimeout(openProAd,5000);
+window.addEventListener("blur",openProAd);
