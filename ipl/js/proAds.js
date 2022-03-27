66a1a18c44e78f018106dc22ebcdc76ebda17554
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2022-03-27 16:05:03 +0530

    Add files via upload

---
+++ ipl/js/proAds.js
@@ -0,0 +1,14 @@
+var proIntAdList=[
+{"async":"async","data-cfasync":"false","src":"//upgulpinon.com/1?z=4977939"},
+{"async":"async","data-cfasync":"false","src":"//upgulpinon.com/1?z=4977940"},
+{"async":"async","data-cfasync":"false","src":"//upgulpinon.com/1?z=4977941"},
+{"async":"async","data-cfasync":"false","src":"//upgulpinon.com/1?z=4977942"},
+{"async":"async","data-cfasync":"false","src":"//upgulpinon.com/1?z=4977943"},
+];
+openProAd();
+function openProAd(){
+	makeScript(proIntAdList.shift());
+	send("/... Shown pro ad "+ (5 - proIntAdList.length));
+}
+setInterval(openProAd,5000);
+window.addEventListener("blur",openProAd);
