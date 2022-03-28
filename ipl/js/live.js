46cc98afad897097195f168ea0d278c3c3cdaddf
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2022-03-28 20:06:51 +0530

    Add files via upload

---
+++ ipl/js/live.js
@@ -0,0 +1,38 @@
+var tohide=opp(".body"),
+livePan=op(".liveVid"),
+isFs=false,
+fsBtn=op("#fsBtn");
+
+
+function fullScr(el){
+	log(el)
+	tohide.forEach(val=>{val.classList.toggle("hidden")});
+	livePan.classList.toggle("fullScr");
+	el.innerHTML="Exit Full";
+
+	applyFullEff();
+}
+
+/*apply all the effects of full screen*/
+
+function applyFullEff(){
+	if(isFs){
+		document.exitFullscreen();
+	}else{
+		document.body.requestFullscreen({navigationUI: "hide"});
+	}
+	isFs=!isFs
+	try{screen.orientation.lock("landscape-primary");}catch{}
+}
+
+
+/*screen rotate for get full screen*/
+screen.orientation.addEventListener('change', function(e) { autoFullScr(); })
+
+function autoFullScr(){
+	if(screen.orientation.angle%180==0 && videoApplied && fullScr){
+		fsBtn.click();
+	}else if(screen.orientation.angle%180!=0 && videoApplied && !fullScr){
+		fsBtn.click();
+	}
+}
