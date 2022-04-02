469f3d4017779afa668bbbb5626631b57b82e18e
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2022-04-02 14:36:57 +0530

    Add files via upload

--- common/js/disableProIntAd.js
+++ common/js/disableProIntAd.js
@@ -1,18 +1,20 @@
 log('file loaded')
 var int=false;
-function disableProInt(){
+function disableProInt(toOpen=false){
 	clearTimeout(int);
 	int=setInterval(()=>{
 		log("came to disable")
 		if(document.body.style.overflow=='hidden'){
+			clearTimeout(int);
+
 			document.body.style.overflow="";
 			var baap=op("#_m8beems").parentElement.parentElement;
 			baap.style.display="none";
 
-			clearTimeout(int);
-			setTimeout(()=>{
-				baap.remove();
-			},5000)
+			if(toOpen){	
+				try{op("#_m8beems iframe ~ div").click();}catch{}
+			}
+			baap.remove();
 		}
 	},100);
 }
