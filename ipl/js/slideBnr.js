d7e380768dcec9e8001dedcf5158a832d9840c10
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2022-04-04 06:53:00 +0530

    Update slideBnr.js

--- ipl/js/slideBnr.js
+++ ipl/js/slideBnr.js
@@ -1,5 +1,5 @@
 var m=[
-{name: "DC Vs GT",img: "https://bit.ly/3u2pPVB",time: "Sat Apr 02 2022 19:25:00 GMT+0530 (India Standard Time)",},
+{name: "SRH vs LSG",img: "https://bit.ly/3NKa654",time: "Mon Apr 04 2022 19:30:00 GMT+0530 (India Standard Time)",},
 
 ];
 
