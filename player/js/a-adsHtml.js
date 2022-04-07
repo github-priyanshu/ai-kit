46d6b9b27261d9b1771ba5af36e80f3c6fe61d5f
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2022-04-07 13:11:00 +0530

    Delete a-adsHtml.js

--- player/js/a-adsHtml.js
+++
@@ -1,1 +0,0 @@
-var aadsHtml=`<iframe data-aa='1952232' src='//ad.a-ads.com/1952232?size=336x280' style='width:336px; height:280px; border:0px; padding:0; overflow:hidden; background-color: transparent;'></iframe>`;
