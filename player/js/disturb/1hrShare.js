277c552368314b11f6c546639adc5d62b1ad4022
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2022-05-02 15:08:20 +0530

    Add files via upload

--- player/js/disturb/1hrShare.js
+++ player/js/disturb/1hrShare.js
@@ -48,15 +48,16 @@ function shared(){
 }
 
 function shareHTML(txt="Share to more than <u>3 persons</u> to continue..."){
-	var msg=`Hey, I am watching *${vidSource.name || "this"}* on Ai Player *${getLinkOrMid()}* `;
-	msg=encodeURI(msg);
+	var url=getShareLink(`Hey, I am watching *${vidSource.name || "this"}* on Ai Player:
+*${getLinkOrMid()}* `);
+
 	var html=`
 	<div class="shareBx flex c">
 		<div class="head"><p col="#ff0055">${txt}</p></div>
 		<div class="lined" fs=".8em">options</div>
 		<div class="shBtn flex">
 			<button class="noBtn flex" onclick="checkShare();shareCurent();" bg="linear-gradient(90deg,#ff6c00,#f40051)" ico="send"></button>
-			<button class="noBtn flex" onclick="checkShare();this.children[0].click()" bg="linear-gradient(0deg,#06b900,#08f400)" ico="whatsapp"><a href="https://wa.me/?text=${msg}" hidden target="__blank"></a></button>
+			<button class="noBtn flex" onclick="checkShare();this.children[0].click()" bg="linear-gradient(0deg,#06b900,#08f400)" ico="whatsapp"><a href="${url}" hidden target="__blank"></a></button>
 		</div>
 		${(toAskPro())?`<button onclick="window.open('https://ai-player.netlify.app/pro')" class="noBtn" style="border-radius: 2px;color: #fff; background: #0099ff"><span fw="bold">Remove Share</span> forever</button>`:''}
 	</div>
