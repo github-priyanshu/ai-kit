f5b22fa73557d44e7eceb0c146959c88e6d7180a
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2022-05-04 14:30:45 +0530

    Update movies.js

--- common/js/movies.js
+++ common/js/movies.js
@@ -39,7 +39,7 @@ var movies=
 ['Black Crab','3N0nd1Q',`${ms2}Black.Crab.2022/Black.Crab.2022.Hindi.English.480p.WEB-DL.ESub-DudeFilms.in.mkv`,3],
 ['Lalitham Sundram','3urOG4b',`${ms}SouthMovies/Lalitham.Sundaram.2022/Lalitham.Sundaram.2022.Hindi.Malayalam.480p.WEB-DL.ESub-DudeFilms.in.mkv`,2],
 ['Jalsa','36v2phW',`${ms}BollyWood/Amazon.Prime/Jalsa.2022/Jalsa.2022.Hindi.480p.WEB-DL.ESub-DudeFilms.in.mkv`,1],
-['Radhe Shyam','3t17ZBI',`${ms}SouthMovies/Radhe.Shyam.2022.CamRip/Radhe.Shyam.2022.Hindi.Dubbed.480p.CAMRip-DudeFilms.in.mkv`,2],
+['Radhe Shyam','3t17ZBI',`${ms2}Radhe.Shyam.2022/Radhe.Shyam.2022.480p.NF.WEB-DL.Esub-DudeFilms.in.mkv`,2],
 ['Etharkkum Thunindhavan','3J3V9s4',`${ms}SouthMovies/Etharkkum.Thunindhavan.2022.CamRip/Etharkkum.Thunindhavan.2022.Hindi.Dubbed.480p.CAMRip-DudeFilms.in.mkv`,2],
 ['Tha Adam Project','3J8FaZQ',`${ms}HollyWood/NetFlix%20Movies/The.Adam.Project.2022/The.Adam.Project.2022.Hindi.English.480p.WEB-DL.ESub-DudeFilms.in.mkv`,3],
 ['Sitimaar','3HUHySi',`${ms}SouthMovies/Seetimaarr.2021/Seetimaarr.2021.UNCUT.Hindi.Telugu.480p.WEB-DL.ESub-DudeFilms.in.mkv`,2],
