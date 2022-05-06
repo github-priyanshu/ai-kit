df3eed616618bbf68ad8a91150de79ebe70ff99c
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2022-05-06 19:13:45 +0530

    Update movieSiteLnk.js

--- common/js/movieSiteLnk.js
+++ common/js/movieSiteLnk.js
@@ -1,4 +1,4 @@
 var ms="https://dl.odcloud02.workers.dev/",
-ms2="https://dl2.odcloud02.workers.dev/OdCloud_Dhub/",
+ms2="https://dl.gdcloud02.workers.dev/0:/BollyWood/",
 seriesDomain="https://dl.gdcloud02.workers.dev/0:/",
 errorMsg='';
