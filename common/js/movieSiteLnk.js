1d8df11174147adf6d4b27d32a1d19e019021d71
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2022-05-08 17:40:27 +0530

    Update movieSiteLnk.js

--- common/js/movieSiteLnk.js
+++ common/js/movieSiteLnk.js
@@ -1,4 +1,4 @@
 var ms="https://dl.odcloud02.workers.dev/",
 ms2="https://dl2.odcloud02.workers.dev/OdCloud_Dhub/",
 seriesDomain="https://dl.gdcloud02.workers.dev/0:/",
-errorMsg='All the movies are not working..:( <br> We are working to resolve the error.';
+errorMsg='';
