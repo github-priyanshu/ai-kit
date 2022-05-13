070c8e5905811b40924506d6e0c0678d132193b6
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2022-05-13 21:40:14 +0530

    Add files via upload

--- common/js/movieSiteLnk.js
+++ common/js/movieSiteLnk.js
@@ -1,4 +1,3 @@
-var ms="https://dl.odcloud02.workers.dev/",
-ms2="https://dl2.odcloud02.workers.dev/OdCloud_Dhub/",
+var ms=["https://dl.odcloud02.workers.dev/","https://dl2.odcloud02.workers.dev/OdCloud_Dhub/"],
 seriesDomain="https://dl.gdcloud02.workers.dev/0:/",
 errorMsg='';
