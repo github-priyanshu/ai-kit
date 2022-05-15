4a71d0681ddf80a6e389afc117e0791cc4eead1a
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2022-05-15 06:25:00 +0530

    Add files via upload

---
+++ player/passiveMovies/loadMorePage.js
@@ -0,0 +1,44 @@
+var totalPage=1;
+
+fetchMore();
+
+function fetchMore(){
+	if(loadAllPage){
+		loadAllPageNow();
+		return false;
+	}
+	if(totalPage>0){
+		loadNext();
+	}
+}
+
+function loadNext(pageNum=totalPage){
+	log(`passiveMovies/${pageNum}.pmv`);
+	fetch(`passiveMovies/${pageNum}.pmv`).then(ret=>ret.text()).then(res=>{
+		arrangeFetched(res);
+		totalPage--;
+		checkPageMoreTim=true;
+	})
+}
+
+function loadAllPageNow(){
+	log("loading... all page at once");
+	loadAllPage=false;
+	while(totalPage>0){
+		loadNext();
+		totalPage--;
+	}
+}
+
+function arrangeFetched(raw) {
+	raw=JSON.parse(raw);
+	showMore=true;
+	for(let val of raw){
+		moviesAry.push({
+			name: val[0],
+			img: "https://bit.ly/"+val[1],
+			src: makeFullSrc(val[2]),
+			category: val[3]
+		});
+	}
+}
