6f02542ba01b9af160626fd2e19dbb770cbbdd92
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2022-05-27 19:24:16 +0530

    Update webSeries.js

--- common/js/webSeries.js
+++ common/js/webSeries.js
@@ -3,6 +3,9 @@ var webseries=
 /*change the episod number to '01' */
 /*[name,bitly img id,totalEpisods,fristEpisodeLink],*/
 
+['Wan Kenobi S1','3NzA22u',2,'__wsdomain/WebSeries/Obi.Wan.Kenobi.S01/Obi.Wan.Kenobi.S01.E01.Hindi.English.720p.10bit.WEB-DL.ESub-DudeFilms.in.mkv'],
+['Stranger Things S04','3NCsdZY',7,'__wsdomain/WebSeries/NetFlix/Stranger.Things.S04/Stranger.Things.S04.E01.Hindi.English.720p.WEB-DL.ESub-DudeFilms.in.mkv'],
+['Nirmal Pathaki Ghar Wapsi S01','3PKljDU',5,'__wsdomain/WebSeries/Sony.Liv/Nirmal.Pathak.Ki.Ghar.Wapsi.S01/Nirmal.Pathak.Ki.Ghar.Wapsi.S01.E01.Hindi.720p.WEB-DL.ESub-DudeFilms.in.mkv'],
 ['Escaype Live S01','3lvQ8y9',7,'__wsdomain/WebSeries/hotstar/Escaype.Live.S01/Escaype.Live.S01.E01.Hindi.720p.WEB-DL.ESub-DudeFilms.in.mkv'],
 ['Panchayat S02','3Mvxen4',8,'__wsdomain/WebSeries/Amazon.Prime/Panchayat.S02/Panchayat.S02.E01.Hindi.720p.WEB-DL.ESub-DudeFilms.in.mkv'],
 ['Modern Love Mumbai S01','3LdGwCH',6,'__wsdomain/WebSeries/Amazon.Prime/Modern.Love.Mumbai.S01/Modern.Love.Mumbai.S01.E01.Hindi.720p.WEB-DL.ESub-DudeFilms.in.mkv'],
