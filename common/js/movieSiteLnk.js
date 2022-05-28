8022fb367a635bf16b7066dae28c9e432b22050a
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2022-05-28 13:03:01 +0530

    Update movieSiteLnk.js

--- common/js/movieSiteLnk.js
+++ common/js/movieSiteLnk.js
@@ -1,3 +1,3 @@
 var ms=["https://dl.odcloud03.workers.dev/","https://dl2.odcloud03.workers.dev/OdCloud_Dhub/"],
 seriesDomain="https://dl.gdcloud02.workers.dev/0:/",
-errorMsg='Unable to play movies... A problem occured. We are solving. ';
+errorMsg='<b>Unable to play movies... A problem occured. We are solving. </b>';
