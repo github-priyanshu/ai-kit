110a39ada3b01b182d2aa462236a9d68c10dfbfd
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2022-05-29 08:29:55 +0530

    Update movieSiteLnk.js

--- common/js/movieSiteLnk.js
+++ common/js/movieSiteLnk.js
@@ -1,3 +1,3 @@
 var ms=["https://dl.odcloud03.workers.dev/","https://dl2.odcloud03.workers.dev/OdCloud_Dhub/"],
 seriesDomain="https://dl.gdcloud02.workers.dev/0:/",
-errorMsg='<b>Unable to play movies... A problem occured. We are solving. </b><br>You can watch some movies like... <i>Bhool Bhulaiyaa 2, Heropanti 2, Runway 34</i>';
+errorMsg='';
