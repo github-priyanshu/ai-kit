fa4b6027efc4682f05dec6a7f8e5fe2d36b0c5f0
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2022-06-04 07:30:49 +0530

    Update movies.js

--- common/js/movies.js
+++ common/js/movies.js
@@ -8,8 +8,7 @@ var movies=
 /*['name',`0>lnk`,mid,'img', cat],*/
 /*['name',`0>lnk`,mid,'img', cat],*/
 
-['Major','http://bit.ly/3meGzEg',`https://dflinks.club/archives/12268#`,1],
-['Sher Shivraj','3M1gdjv',`1>Sher.Shivraj.2022.Marathi/Sher.Shivraj.2022.Marathi.480p.WEB-DL.ESub-DudeFilms.in.mkv`,1],
+['Major','3teGmok',`https://dl.gdcloud03.workers.dev/0:/BollyWood/Major.2022.CamRip/Major.2022.Hindi.480p.CAMRip-DudeFilms.in.mkv`,1],['Sher Shivraj','3M1gdjv',`1>Sher.Shivraj.2022.Marathi/Sher.Shivraj.2022.Marathi.480p.WEB-DL.ESub-DudeFilms.in.mkv`,1],
 ['Acharya','3LTrWAI',`1>Acharya.2022.HQ.Hindi/Acharya.2022.HQ.Hindi.Dubbed.480p.HDRip-DudeFilms.in.mkv`,2],
 ['Anek','3ND0dp9',`1>Anek.2022.CamRip/Anek.2022.Hindi.480p.CAMRip-DudeFilms.in.mkv`,1],
 ['Runway 34 HD','3wWbFXa',`https://dl2.odcloud03.workers.dev/OdCloud_Dhub/Runway.34.2022/Runway.34.2022.Hindi.480p.WEB-DL.ESub-DudeFilms.in.mkv`,1],
