37f1219491334714288dcc125878427e6c0b33e9
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2022-06-12 10:20:50 +0530

    Update movies.js

--- common/js/movies.js
+++ common/js/movies.js
@@ -8,6 +8,7 @@ var movies=
 /*['name',`0>lnk`,mid,'img', cat],*/
 /*['name',`0>lnk`,mid,'img', cat],*/
 
+['Vikram movie','3aEUvot',`https://s18.drop.download:182/d/elcwvnj44tcdvbi43255dvic2vl3v5hffoiteolawa4aeuibbwstvqm2tjfzgg6djqqwjvi2/Vikram.2022.Hindi.Dubbed.480p.V3.CAMRip-DudeFilms.in.mkv`,1],
 ['Charlie 777','3NO3DWw',`https://s14.drop.download:182/d/fhc27ab34tcdvbi4z255vhqyzu4f6kdrgkrnvn5setweoex7b6f7m3aqpcaozztznruwuzrl/777.Charlie.2022.Dual.Audio.Hindi.Tamil.480p.CAMRip-DudeFilms.in.mkv`,1],
 //['Ardh movie','3O4y22A',`https://s01.drop.download:182/d/ghcvjmb34tcdvbi4zw55dhizsthi4dfn3ou6bn745vfn227u3agc7dasepmydckuucu4knqn/Ardh.2022.Hindi.720p.WEB-DL.ESub-DudeFilms.in.mkv`,1],
 ['Samrat Prithviraj','38MMBsv',`https://dl.gdcloud03.workers.dev/0:/BollyWood/Samrat.Prithviraj.2022.CamRip/Samrat.Prithviraj.2022.Hindi.480p.CAMRip-DudeFilms.in.mkv`,1],
