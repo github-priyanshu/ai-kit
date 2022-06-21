afae890ed3192ce7a6a0975f79530d22b728ebb5
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2022-06-21 12:09:34 +0530

    Update movieSiteLnk.js

--- common/js/movieSiteLnk.js
+++ common/js/movieSiteLnk.js
@@ -1,3 +1,3 @@
 var ms=["https://dl.odcloud03.workers.dev/","https://dl2.odcloud03.workers.dev/OdCloud_Dhub/"],
 seriesDomain="https://dl.gdcloud03.workers.dev/0:/",
-errorMsg="Don't Skip in <u>Jan Hit Mein Jari</u> movie. We Recommend you to download it directly.";
+errorMsg="We encountered a server ERROR 500. Resolving Soon. Join our telegram for UPDATES"
