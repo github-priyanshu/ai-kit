95a41b3b948081a6d72aa1eddfed881995239698
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2022-06-23 12:31:24 +0530

    Update movieSiteLnk.js

--- common/js/movieSiteLnk.js
+++ common/js/movieSiteLnk.js
@@ -1,3 +1,3 @@
 var ms=["https://dl.odcloud03.workers.dev/","https://dl2.odcloud03.workers.dev/OdCloud_Dhub/"],
 seriesDomain="https://dl.gdcloud03.workers.dev/0:/",
-errorMsg="We encountered a server ERROR 500. Resolving Soon. Join our telegram for UPDATES"
+errorMsg=""
