258b30b62678c1f62b1a710ca84a3bdaff4211b5
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2022-06-24 19:01:41 +0530

    Update movies.js

--- common/js/movies.js
+++ common/js/movies.js
@@ -8,6 +8,8 @@ var movies=
 /*['name',`0>lnk`,mid,'img', cat],*/
 /*['name',`0>lnk`,mid,'img', cat],*/
 
+['Jug Jugg Jeeyo','3HNJLk0',`2>BollyWood/Jug.Jugg.Jeeyo.2022.CamRip/Jug.Jugg.Jeeyo.2022.Hindi.480p.HD.CAMRip-DudeFilms.in-.mkv`,1],
+['Forensic','3bvNcQA',`2>BollyWood/Forensic.2022/Forensic.2022.Hindi.480p.WEB-DL.ESub-DudeFilms.in.mkv`,3],
 //['Jan Hit Mein Jari','3QAor5K',`https://bit.ly/3N3RiMQ`,1],
 ['Bhool Bhulaiyaa 2  HD','3zNtk5p',`1>Bhool.Bhulaiyaa.2.2022/Bhool.Bhulaiyaa.2.2022.Hindi.480p.NF.WEB-DL.ESub-DudeFilms.in.mkv`,1],
 ['Nikamma','3HBFIHm',`https://dl.gdcloud03.workers.dev/0:/BollyWood/Nikamma.2022.HDCampRip/Nikamma.2022.Hindi.480p.HDCAM-DudeFilms.in.mkv`,1],
