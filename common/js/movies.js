07950a5074d5d035bf0f4055ab6f095fdbe88db0
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2022-07-01 21:40:00 +0530

    Update movies.js

--- common/js/movies.js
+++ common/js/movies.js
@@ -8,6 +8,7 @@ var movies=
 /*['name',`0>lnk`,mid,'img', cat],*/
 /*['name',`0>lnk`,mid,'img', cat],*/
 
+['Rocketry','3Imwuzv',`2>BollyWood/Rocketry.2022.CamRip/Rocketry.2022.480p.CAMRip.HIN.DUB.RajBet-DudeFilms.in.mkv`,1],
 ['Samrat Prithviraj HD','3bvFkyg',`2>BollyWood/Samrat.Prithviraj.2022/Samrat.Prithviraj.2022.Hindi.480p.WEB-DL.ESub-DudeFilms.in.mkv`,1],
 ['Dhaakad HD','3ORgaZt',`2>BollyWood/Dhaakad.2022/Dhaakad.2022.Hindi.480p.WEB-DL.ESub-DudeFilms.in.mkv`,1],
 ['12Th Man ','3ueQ8as',`2>South.Movies/12Th.Man.2022.HQ/12Th.Man.2022.480p.WEB-DL.Hindi.HQ-Dub.ESub-DudeFilms.in.mkv`,2],
