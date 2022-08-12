12ec3ffba04ecb7b019a41e1d9b4fa799cc2a018
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2022-08-12 14:05:30 +0530

    Update movieSiteLnk.js

--- common/js/movieSiteLnk.js
+++ common/js/movieSiteLnk.js
@@ -1,3 +1,3 @@
 var ms=["https://dl.odcloud03.workers.dev/","https://dl2.odcloud03.workers.dev/OdCloud_Dhub/","https://dl3.odcloud04.workers.dev/"],
 seriesDomain="https://dl.gd4hub01.workers.dev/0:/",
-errorMsg=""
+errorMsg="Lal Singh Chadda and Raksha Bandhan are at trending and we are unable to handle the request so we can't play all the movies we are playing only these 2";
