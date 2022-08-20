8193b54bf05833654ecfb24c8e1d871458f7653a
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2022-08-20 12:49:13 +0530

    Update movieSiteLnk.js

--- common/js/movieSiteLnk.js
+++ common/js/movieSiteLnk.js
@@ -1,3 +1,3 @@
 var ms=["https://dl.odcloud04.workers.dev/","https://dl2.odcloud03.workers.dev/OdCloud_Dhub/","https://dl3.odcloud04.workers.dev/"],
 seriesDomain="https://dl.gd4hub01.workers.dev/0:/",
-errorMsg="";
+errorMsg="Unable to handle the requests...!! <br>We are only playing <i>Laal Singh Chadda</i> and <i>Raksha Bandhan</i>";
