facc9d574c4c27f7373924bd94aa4d6b394c58a4
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2022-08-26 22:20:15 +0530

    Update movieSiteLnk.js

--- common/js/movieSiteLnk.js
+++ common/js/movieSiteLnk.js
@@ -1,3 +1,3 @@
 var ms=["https://dl.odcloud04.workers.dev/","https://dl2.odcloud03.workers.dev/OdCloud_Dhub/","https://dl3.odcloud04.workers.dev/"],
 seriesDomain="https://dl.gd4hub01.workers.dev/0:/",
-errorMsg="";//"Unable to handle the requests...!! <br>We are only playing <i>Laal Singh Chadda</i> and <i>Raksha Bandhan</i>";
+errorMsg="<a href='https://t.me/bhojourifilm'>भोजपूरी फिल्म</a>"
