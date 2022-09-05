c82ff7b8a7a65146dcf7d6b8f6faaf77cf7411db
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2022-09-05 17:44:28 +0530

    Update movieSiteLnk.js

--- common/js/movieSiteLnk.js
+++ common/js/movieSiteLnk.js
@@ -1,3 +1,3 @@
 var ms=["https://dl.odcloud04.workers.dev/","https://dl2.odcloud03.workers.dev/OdCloud_Dhub/","https://dl3.odcloud04.workers.dev/"],
 seriesDomain="https://dl.gd4hub03.workers.dev/0:/",
-errorMsg="Now we are only playing Cutputtli Movie<br><h1><a href='https://t.me/bhojourifilm'>भोजपूरी फिल्म</a></h1>"
+errorMsg="<h1><a href='https://t.me/bhojourifilm'>भोजपूरी फिल्म</a></h1>"
