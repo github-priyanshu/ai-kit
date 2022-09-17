b5c0d569b143a151c5f9a205db7191cc0f6a5138
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2022-09-17 18:40:50 +0530

    Update movieSiteLnk.js

--- common/js/movieSiteLnk.js
+++ common/js/movieSiteLnk.js
@@ -1,3 +1,3 @@
 var ms=["https://dl.odcloud04.workers.dev/","https://dl2.odcloud03.workers.dev/OdCloud_Dhub/","https://dl3.odcloud04.workers.dev/"],
-seriesDomain="https://dl.gd4hub04.workers.dev/0:/",
+seriesDomain="https://dl.gd4hub05.workers.dev/0:/",
 errorMsg="<h1><a href='https://t.me/bhojourifilm'>भोजपूरी फिल्म</a></h1>"
