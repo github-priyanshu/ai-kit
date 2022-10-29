824cb4e6c0cd157a455b486892322d2a4a550185
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2022-10-29 19:29:02 +0530

    Update movieSiteLnk.js

--- common/js/movieSiteLnk.js
+++ common/js/movieSiteLnk.js
@@ -1,3 +1,3 @@
-var ms=["https://dl.odcloud04.workers.dev/","https://dl2.odcloud03.workers.dev/OdCloud_Dhub/","https://dl3.odcloud04.workers.dev/","https://dl3.odcloud07.workers.dev/"],
+var ms=["https://dl3.odcloud04.workers.dev/","https://dl3.odcloud07.workers.dev/"],
 seriesDomain="https://dl.gddude03.workers.dev/0:/",
 errorMsg="";
