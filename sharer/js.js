097690a048eb9d0fd17a8f993fe590c04a4c4445
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2022-10-31 18:52:07 +0530

    Add files via upload

---
+++ sharer/js.js
@@ -0,0 +1,131 @@
+var autoPaste=opp(".autoPaste");
+
+autoPaste.forEach(val=>{
+	val.addEventListener("focus",()=>{
+		if(val.value==""){
+			navigator.clipboard.readText().then(ret=>{
+				val.value=ret || " ";
+			});
+		}
+	});
+
+	val.addEventListener("focus",()=>{
+		if(val.value.startsWith("http") && !val.getAttribute("tried")){
+			val.setAttribute("tried",1);
+			document.body.click();
+		}
+	})
+})
+
+function copy(txt){
+	let elem=document.createElement("textarea");
+	document.body.insertAdjacentElement("beforeend",elem)
+	elem.value=txt;
+	elem.select();
+	elem.setSelectionRange(0, 99999999999999); 
+	document.execCommand("copy");
+	navigator.clipboard.writeText(elem.value);
+	elem.remove();
+}
+
+
+/*sTART GENERAL*/
+var obj={
+	sharedBy:'',
+	movie:'',
+	link: 'https://ai-player.netlify.app'
+},
+shareNameBx=op(".shareName"),
+mvNameBx=op(".mvName"),
+msgTxt=op("textarea.txt");
+
+function getFullUrl(){
+	var shtxt=""
+	if(obj.sharedBy!=''){
+		shtxt="?sh="+obj.sharedBy;
+	}
+	if(obj.movie!=''){
+		shtxt="?mid="+obj.movie;
+	}
+	if(obj.sharedBy!='' && obj.movie!=''){
+		shtxt="?sh="+obj.sharedBy+"&mid="+obj.movie;
+	}
+	return obj.link+shtxt;
+}
+
+/*MAKING SHAREING SETTINGS*/
+var html=`<button class="name" onclick="setShare('')">None</button>`;
+shareName.map((val,i)=>{
+	html+=`<button class="name" onclick="setShare('${i+1}')">${val}</button>`;
+})
+shareNameBx.innerHTML=html;
+
+function setShare(i){
+	obj.sharedBy=i;
+	changeTxtMsg();
+}
+
+
+/*COPY TXT MSG*/
+changeTxtMsg();
+function changeTxtMsg(){
+	var uri=getFullUrl();
+
+	var txt=`All new MOVIES & WebSeries
+Download & Watch Free
+
+${uri}
+
+Some Populars:
+
+Har Har Mahadev
+Ram Setu
+Hindutva
+
+${uri}`;
+	msgTxt.value=txt;
+}
+
+/*MOVIE CHOOSER*/
+html=`<button class="name" onclick="setMovie('')">None</button>`;
+
+for(let i=0; i<10; i++){
+	var val=moviesAry[i];
+	html+=`<button class="name" onclick="setMovie('${val.mid}')">${val.name}</button>`;
+}
+var htmlx="<option value=''>See More</option>";
+moviesAry.map(val=>{
+	htmlx+=`<option value="${val.mid}">${val.name}</option>`;
+})
+html+=`<select id="movieMid" onchange="setMovie(this.value)">${htmlx}</select>`
+
+mvNameBx.innerHTML=html;
+
+function setMovie(i){
+	obj.movie=i;
+	changeTxtMsg();
+}
+
+
+/*CHOOSE TO COPY SECTIONS*/
+function sms(){
+	var txt=`Watxh & Downloxd Now..!!
+*"${movies[obj.movie].name}"*
+👇👇👇
+${getFullUrl()}
+`
+copy(txt);
+}
+
+
+/*DOWNLOAD LINK*/
+function downloadLink(){
+	var val=movies[obj.movie];
+	copy(`https://ai-movie-download.netlify.app?lnk=${JSON.stringify({name:val.name,src:val.src,altLnk:val.altLnk})}`);
+}
+
+/*REDIRECT ARTICLE LINK*/
+function redirectArticleLink() {
+	var objx={mid:obj.movie};
+	copy(`https://ai-article.netlify.app/page/bhool%20bhulaiyaa%202?redirect=`+JSON.stringify(objx));
+}
