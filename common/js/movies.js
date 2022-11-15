db796807f7c77ac5241eb9748cb2da0310c4b712
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2022-11-15 15:33:55 +0530

    Update movies.js

--- common/js/movies.js
+++ common/js/movies.js
@@ -8,6 +8,7 @@ var movies=
 /*['name',`0>lnk`,mid,'img', cat],*/
 /*['name',`0>lnk`,mid,'img', cat],*/
 
+['Pakka Commercial','3ttwAP5',`1>South.Movies/Pakka.Commercial.2022/Pakka.Commercial.2022.Hindi.Telugu.480p.WEB-DL.ESub-DudeFilms.in.mkv`,2,'l35hghpaj7iw'],
 ['Sardar','3X3ipgY',`1>South.Movies/Sardar.2022.HQ/Sardar.2022.Hindi.(HQ.Dubbed).480p.HDRip-DudeFilms.in.mkv`,2,'eefgjqo5xvh1'],
 ['Thai Massage CamRip','3UP5wW4',`1>BollyWood/Thai.Massage.2022.CamRip/Thai.Massage.2022.Hindi.480p.CAMRip-DudeFilms.in.mkv`,1,'i2encs1ci76f'],
 ['Uunchai CamRip','3GiDQ89',`1>BollyWood/Uunchai.2022.CamRip/Uunchai.2022.Hindi.480p.CAMRip-DudeFilms.in.mkv`,1,'7hgzbla4561h'],
