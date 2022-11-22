b15d7cd6f769020f22d7d46fe880541a494a63f5
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2022-11-22 18:17:31 +0530

    Update movies.js

--- common/js/movies.js
+++ common/js/movies.js
@@ -8,6 +8,7 @@ var movies=
 /*['name',`0>lnk`,mid,'img', cat],*/
 /*['name',`0>lnk`,mid,'img', cat],*/
 
+['Love Today','3OvY8x0',`https://dl.dudefan04.workers.dev/0:/South.Movies/Love.Today.2022.HQ/Love.Today.2022.Hindi.(HQ).Tamil.480p.HDRip.HC-ESub-DudeFilms.in.mkv`,2,'1skqpjh0ac6n'],
 ['Mister Mummy','3V9KNwd',`https://dl3.oddfun02.workers.dev/BollyWood/Mister.Mummy.2022.CamRip/Mister.Mummy.2022.Hindi.480p.CAMRip-DudeFilms.in.mkv`,1,'sxc2tgdsox68'],
 ['Drishyam 2 HD','3hXrZBX',`https://dl3.oddfun02.workers.dev/BollyWood/Drishyam.2.2022.CamRip/Drishyam.2.2022.Hindi.480p.CAMRip-DudeFilms.in.mkv`,1,'6tth0n6ek39w'],
 ['Godfather HD','3U5Q1sd',`https://dl3.oddfun02.workers.dev/South.Movies/Godfather.2022/Godfather.2022.Hindi.Dubbed.480p.WEB-DL.ESub-DudeFilms.in.mkv`,2,'txpw22d729vj'],
