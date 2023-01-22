1224e19096172c5bbf2679e1382c427716f37bab
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2023-01-22 11:01:53 +0530

    Update pwa.js

--- player/specialMovie/pwa.js
+++ player/specialMovie/pwa.js
@@ -14,3 +14,10 @@ window.addEventListener('beforeinstallprompt', (e) => {
   downBtn.classList.add("active")
   readyToDownload=true;
 })
+downBtn.onclick=(e) => {
+  deferredPrompt.prompt(); deferredPrompt.userChoice.then((choiceResult) => {
+    if (choiceResult.outcome === 'accepted') {
+      // console.log('User accepted the install prompt');
+    } else { /*console.log('User dismissed the install prompt');*/ }
+  });
+};
