32b8bf42f8d5b49a5dcf7887a0d2923a0008283c
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2023-01-22 10:38:50 +0530

    Add files via upload

---
+++ player/specialMovie/pwa.js
@@ -0,0 +1,16 @@
+let readyToDownload=false;
+if ('serviceWorker' in navigator) {
+   window.addEventListener('load', function () { navigator.serviceWorker.register('../sw.js').then(function (registration) {log(registration)}, function (err) {
+   	log(err)
+   }); });
+}
+let deferredPrompt;
+var downBtn=op(".downBtn");
+log(downBtn)
+// downBtn.classList.remove("active")
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault(); deferredPrompt = e;
+  downBtn.classList.add("active")
+  readyToDownload=true;
+})
