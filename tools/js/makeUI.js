93a50110e116c3bd8fb230e04f35fb9869929760
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2023-02-04 13:53:04 +0530

    Add files via upload

--- tools/js/makeUI.js
+++ tools/js/makeUI.js
@@ -13,6 +13,9 @@ document.body.insertAdjacentHTML("afterbegin",`
         <div class="border" w="0" m="5"></div>
         <p class="lined" fs=".8em">No more external app needed now.</p>
       </div>
+      <iframe data-aa='2164055' src='//ad.a-ads.com/2164055?size=320x100' style='width:320px; height:100px; border-radius: 10px; border:0px; padding:0; overflow:hidden; background-color: transparent;'></iframe>
+      <div class="border" w="70" m="50"></div>
+
       <div id="pan" class="flex c">
 
       </div>
--- tools/js/makeWin.js
+++ tools/js/makeWin.js
@@ -5,7 +5,6 @@ document.body.insertAdjacentHTML("beforeend",`
       <div class="head flex">
         <img src="tools/img/toolDesc.png" alt="">
       </div>
-      <iframe data-aa='2164044' src='//ad.a-ads.com/2164044?size=300x250' style='width:300px; height:250px; border-radius: 10px;border:0px; padding:0; overflow:hidden; background-color: transparent;'></iframe>
       <button class="noBtn flex downBtn" ico="download"><span>Download All In 1 App <b>1MB</b></span></button>
     </div>
 
