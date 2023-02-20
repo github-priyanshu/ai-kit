c565a79be6037a85e949f4e829c743a7e9214728
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2023-02-20 14:28:11 +0530

    Update 1hrShare.js

--- player/js/disturb/1hrShare.js
+++ player/js/disturb/1hrShare.js
@@ -1,5 +1,5 @@
 var lastShare=localStorage.getItem("lastShare") || false,nowVid,
-extLink="https://youtube.com/watch?v=6YL5_PGGhQ8",
+extLink="https://money-now.netlify.app",
 shareNum=Number(localStorage.getItem("shareNum")) || 0;
 
 var hrShare={
