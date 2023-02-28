f34f3977f6623fd50684eecec6e67d92868173e6
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2023-02-28 16:49:59 +0530

    Update movieSiteLnk.js

--- common/js/movieSiteLnk.js
+++ common/js/movieSiteLnk.js
@@ -1,4 +1,4 @@
-var seriesDomain="https://dl.bagos16551.workers.dev/0:/",
+var seriesDomain="https://dl.ligogex266.workers.dev/0:/",
     ms=["https://dl3.odcloud04.workers.dev/","https://dl3.oddfun01.workers.dev/",seriesDomain],
 
 errorMsg="";
