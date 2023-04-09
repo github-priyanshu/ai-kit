014da1ea6de15d1f66e4e78857f6d3a72d032e7a
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2023-04-09 14:59:29 +0530

    Add files via upload

--- common/js/sharedBy.js
+++ common/js/sharedBy.js
@@ -1,1 +1,6 @@
-let shareName=["Pathaan","Awesome Code","Chahat","Telegram","Prashant","AshishSharma","Sinchan","Aman","fb","insta","other","qr code","person","code viewer site","ai kit","WISH","User share","Spin&Win","MallQr","ShortShare","Down Share"];
+let shareName=["Pathaan","Awesome Code","Chahat","Telegram","Prashant","AshishSharma","Sinchan","Atul","fb","insta","other","qr code","person","code viewer site","ai kit","WISH","User share","Spin&Win","MallQr","ShortShare","Down Share"];
+try{
+  if(sh && !localStorage.getItem('aiSharedBy')){
+    localStorage.setItem('aiSharedBy',shareName[sh-1]);
+  }
+}catch(){}
