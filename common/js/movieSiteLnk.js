f5ce0e11550f663a6d0b0757262a7ee09dbca4ee
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2023-04-20 21:51:25 +0530

    Update movieSiteLnk.js

--- common/js/movieSiteLnk.js
+++ common/js/movieSiteLnk.js
@@ -1,4 +1,4 @@
-var seriesDomain="https://dl.kolebo83261737.workers.dev/0:/",
+var seriesDomain="https://dl.woham423435037.workers.dev/0:/",
     ms=["https://dl3.odcloud04.workers.dev/","https://dl3.oddfun01.workers.dev/",seriesDomain],
 
 errorMsg="";
