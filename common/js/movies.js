1fe96ad55a7b249cfc3837b3a38928cc09dd6327
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2023-04-21 14:34:32 +0530

    Update movies.js

--- common/js/movies.js
+++ common/js/movies.js
@@ -8,6 +8,7 @@ var movies=
 /*['name',`0>lnk`,mid,'img', cat],*/
 /*['name',`0>lnk`,mid,'img', cat],*/
 
+['Kisi Ka Bhai Kisi Ki Jaan','3UXmL96',`https://drop.download/a7gna82gubrt`,1,'a7gna82gubrt'],
 ['Selfiee  HD','3LkcWyP',`2>Movies/Selfiee.2023/Selfiee.2023.Hindi.480p.WEBDL.ESub-DudeFilms.in.mkv`,1,'e820hohwnayk'],
 ['AntMan and the Wasp Quantumania HD (English)','3mTtiVG',`2>English.Movies/AntMan.and.the.Wasp.Quantumania.2023/AntMan.and.the.Wasp.Quantumania.2023.English.480p.WEBDL.ESub-DudeFilms.in.mkv`,3,'0mti2pg2qgk7'],
 ['Iron Sky The Comming Race','3LbKmPO',`https://drop.download/sogdm277w7f8`,3,'sogdm277w7f8'],
