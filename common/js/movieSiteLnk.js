4ec4472048802d3b1674fee780df5c7b65ff36e1
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2023-05-13 16:52:30 +0530

    Update movieSiteLnk.js

--- common/js/movieSiteLnk.js
+++ common/js/movieSiteLnk.js
@@ -1,4 +1,4 @@
-var seriesDomain="https://dl.yocima74984581.workers.dev/0:/",
+var seriesDomain="https://dl.coxom302087418.workers.dev/0:/Movies/Bholaa.",
     ms=["https://dl3.odcloud04.workers.dev/","https://dl3.oddfun01.workers.dev/",seriesDomain],
 
 errorMsg="";
